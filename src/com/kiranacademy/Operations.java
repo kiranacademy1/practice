aa424b1355a53e54562c360fc211f6c55650feff
anirudhawarkad <anirudhawarkad.17@gmail.com>
2025-06-13 17:57:21 +0530

    factorial

--- src/com/kiranacademy/Operations.java
+++ src/com/kiranacademy/Operations.java
@@ -31,8 +31,18 @@ public class Operations {
 	}
 
 	public void factorial() {
+		
+		   int num = 5;  
+	        int fact = 1;
 
-	}
+	        for (int i = 1; i <= num; i++) {
+	            fact *= i;
+	        }
+
+	        System.out.println("Factorial of " + num + " is: " + fact);
+	    
+	    }
+	    
 
 	public void squareRoot() {
 
