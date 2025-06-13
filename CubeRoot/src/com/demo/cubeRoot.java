5f07e238f11ef9c27e501b63e49e935f64b0bc6e
Taufik-Mulla <mullataufik0313@gmail.com>
2025-06-13 17:21:59 +0530

    Taufik code
    taufik code

---
+++ CubeRoot/src/com/demo/cubeRoot.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class cubeRoot {
+
+	public static void main(String[] args) {
+		 int num = 7;
+		 int ans = num*num*num;
+		 System.out.println("Cube root is:"+ans);		 
+	}
+
+}
