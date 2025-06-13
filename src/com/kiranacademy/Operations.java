2d0ea1fc7a5b2f2e24439062c25a33137c9498d4
kiranacademy1 <kiranacademy1@gmail.com>
2025-06-13 16:58:26 +0530

    project is ready to develope

---
+++ src/com/kiranacademy/Operations.java
@@ -0,0 +1,56 @@
+package com.kiranacademy;
+
+public class Operations {
+
+	public void add() {
+
+	}
+
+	public void subtract() {
+
+	}
+
+	public void multiply() {
+
+	}
+
+	public void divide() {
+
+	}
+
+	public void modulus() {
+
+	}
+
+	public void exponent() {
+
+	}
+
+	public void factorial() {
+
+	}
+
+	public void squareRoot() {
+
+	}
+
+	public void cubeRoot() {
+
+	}
+
+	public void absolute() {
+
+	}
+
+	public void logarithm() {
+
+	}
+
+	public void average() {
+
+	}
+
+	public void square() {
+
+	}
+}
